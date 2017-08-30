4845258d5c32a78bdfdb8c13a21c62a7fd4e401d
Dirk Geurs <dirklectisch@mac.com>
2017-08-30 14:50:05 +0200

    Basic text queries from :search remote

--- src/zomdemo/core.cljs
+++ src/zomdemo/core.cljs
@@ -1,5 +1,5 @@
 (ns zomdemo.core
-  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]])
   (:require [goog.dom :as gdom]
             [om.dom :as dom]
             [om.next :as om :refer [defui]]
@@ -10,16 +10,6 @@
            [goog.net Jsonp]))
 
 (enable-console-print!)
-
-(def base-url "https://www.entoen.nu/api/search?text=")
-
-(defn jsonp
-  ([uri]
-   (jsonp (chan) uri))
-  ([c uri]
-   (let [gjsonp (Jsonp. (Uri. uri))]
-     (.send gjsonp nil #(put! c %))
-     c)))
 
 ;; parser
 
@@ -37,12 +27,77 @@
 
 ;; components
 
+(defn result-list [results]
+  (dom/ul #js {:key "result-list"}
+    (map #(dom/li #js {:key %} %) results)))
+
+(defn search-field [this text]
+  (dom/input
+    #js {:key "search-field"
+         :value text
+         :onChange (fn [e]
+                     (om/set-query! this
+                       {:params {:text (.. e -target -value)}}))}))
+
+(defui SearchWidget
+  static om/IQueryParams
+  (params [_]
+    {:text ""})
+  static om/IQuery
+  (query [_]
+    '[(:search/results {:text ?text})])
+  Object
+  (render [this]
+    (let [{:keys [search/results]} (om/props this)]
+      (dom/div nil
+        (dom/h2 nil "Search")
+        (cond->
+          [(search-field this (:text (om/get-params this)))]
+          (not (empty? results)) (conj (result-list results)))))))
+
+;; remote sync
+
+(def base-url "https://www.entoen.nu/api/search?text=")
+
+(defn jsonp
+  ([uri]
+   (jsonp (chan) uri))
+  ([c uri]
+   (let [gjsonp (Jsonp. (Uri. uri))]
+     (.send gjsonp nil #(put! c %))
+     c)))
+
+(defn search-loop [c]
+  (go-loop [[text cb] (<! c)]
+    (let [results (<! (jsonp (str base-url text)))]
+      (cb {:search/results results}))
+    (recur (<! c))))
+
+(defn send-to-chan [c]
+  (fn [{:keys [search]} cb]
+    (when search
+      (let [{[search] :children} (om/query->ast search)
+            text (get-in search [:params :text])]
+        (put! c [text cb])))))
+
+(def send-chan (chan))
+
+(search-loop send-chan)
+
 ;; reconciler
 
 (def default-state {:search/results []})
+
+(def reconciler
+  (om/reconciler
+    {:state   default-state
+     :parser  parser
+     :send    (send-to-chan send-chan)
+     :remotes [:search]}))
+
+(om/add-root! reconciler SearchWidget (gdom/getElement "app"))
 
 (defn on-js-reload [])
 ;; optionally touch your app-state to force rerendering depending on
 ;; your application
 ;; (swap! app-state update-in [:__figwheel_counter] inc)
-
--- src/zomdemo/repl.cljs
+++ src/zomdemo/repl.cljs
@@ -1,6 +1,7 @@
 (ns zomdemo.repl
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [zomdemo.core :as core :refer [jsonp base-url parser default-state]]
+            [om.next :as om]
             [cljs.core.async :refer [<!]]))
 
 (defn try-request [text]
@@ -11,3 +12,9 @@
   ([state query] (parser {:state state} query))
   ([query] (try-query (atom default-state) query))
   ([] (try-query '[(:search/results {:text "canon"})])))
+
+(defn inspect-state []
+  (deref (om/app-state core/reconciler)))
+
+(defn inspect-query []
+  (om/get-query (om/app-root core/reconciler)))
